f530d25cb4a2463ff64dc50ef0c0dd272960a204
Silas Brack <s174433@student.dtu.dk>
2022-10-17 15:15:56 +0200

    Fixed some settings, added visualization.

--- visualization/visualize_regression.py
+++ visualization/visualize_regression.py
@@ -1,19 +1,50 @@
+import json
+import os
 from pathlib import Path
 from typing import List
 
-from matplotlib import animation
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import torch
+from dotenv import load_dotenv
 from hydra.utils import instantiate
+from matplotlib import animation
 from omegaconf import DictConfig, OmegaConf
+
+import wandb
+
+
+def get_run(run_path: str):
+    run_id = json.load(open(f"{run_path}/.wandb.json", "r"))["run_id"]
+    api = wandb.Api()
+    entity, project = os.getenv("WANDB_ENTITY"), os.getenv("WANDB_PROJECT")
+    run = api.run(entity + "/" + project + "/" + run_id)
+    return run
+
+
+def get_run_data(run) -> pd.DataFrame:
+    history = run.scan_history()
+    df = pd.DataFrame(history)
+    return df
+
+
+def plot_loss_curves(df: pd.DataFrame, ax: plt.Axes):
+    pdf_epoch = df[["trainer/global_step", "epoch"]].groupby("epoch").nth(0).reset_index()
+    pdf_epoch = pdf_epoch.loc[range(0, len(pdf_epoch), 2)]
+
+    (line,) = ax.plot(df["trainer/global_step"], df["train/loss"])
+    ax.plot(df["trainer/global_step"], df["val/loss"], marker="o", color=line.get_color())
+    ax.set_xticks(pdf_epoch["trainer/global_step"], pdf_epoch["epoch"])
 
 
 def format_number(data_value, indx):
     if data_value >= 1_000_000:
         formatter = "{:1.1f}M".format(data_value * 0.000_001)
-    else:
+    elif data_value >= 1_000:
         formatter = "{:1.0f}k".format(data_value * 0.001)
+    else:
+        formatter = "{:1.0f}".format(data_value)
     return formatter
 
 
@@ -25,7 +56,7 @@ def get_splits(path: str):
     params_per_layer = list(filter(lambda x: x > 0, params_per_layer))
     num_layers = len(params_per_layer)
     param_limits = [0] + np.cumsum(params_per_layer).tolist()
-    # param_limits[-1] -= 1
+    param_limits = (np.array(param_limits) - 0.5).tolist()
     return param_limits, num_layers
 
 
@@ -54,7 +85,7 @@ def visualize_hessians_diag(run_paths: List[str]):
     splits: List[List[int]] = [s[0] for s in splits_layers]
     num_layers: List[int] = [s[1] for s in splits_layers]
 
-    fig, ax = plt.subplots(nrows=5, ncols=2, sharey="row", figsize=(10, 10))
+    fig, ax = plt.subplots(nrows=5, ncols=2, sharey="row", figsize=(14, 10))
     epoch = 2
     struct = "diag"
     for i, (num_layer, run_path, split) in enumerate(zip(num_layers, run_paths, splits)):
@@ -87,29 +118,40 @@ def create_animation(run_paths: List[str], n_epochs: int, struct: str):
     splits: List[List[int]] = [s[0] for s in splits_layers]
     num_layers: List[int] = [s[1] for s in splits_layers]
 
-    fig, axs = plt.subplots(nrows=len(run_paths), ncols=2, sharey="row", figsize=(10, 10))
+    fig, axs = plt.subplots(nrows=len(run_paths), ncols=3, figsize=(15, 10))
     vmin, vmax = get_min_max_value(run_paths, n_epochs)
 
     def init_plots(axs):
         lines = []
         for i, ax in enumerate(axs):
-            ax_opt, ax_unopt = ax
+            ax_opt = ax[0]
+            ax_unopt = ax[1]
+            ax_loss = ax[2]
+            run = get_run(run_paths[i])
+            # num_layers = json.loads(run.json_config)["num_layers"]["value"]
+            df = get_run_data(run)
+
+            plot_loss_curves(df, ax_loss)
+            ax_loss.set_xlabel("Epoch")
+            ax_loss.set_ylabel("Loss")
 
             lines.append(ax_opt.plot([], [])[0])
-            ax_opt.set_title(f"{i+1} layers, optimized")
+            ax_opt.set_title(f"{num_layers[i]} layers, optimized")
             ax_opt.set_ylabel("Posterior precision")
             ax_opt.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, pos: format_number(x, pos)))
+            ax_opt.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, pos: format_number(x, pos)))
 
             lines.append(ax_unopt.plot([], [])[0])
-            ax_unopt.set_title(f"{i+1} layers, unoptimized")
+            ax_unopt.set_title(f"{num_layers[i]} layers, unoptimized")
             ax_unopt.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, pos: format_number(x, pos)))
+            ax_unopt.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, pos: format_number(x, pos)))
 
             for j, s in enumerate(splits[i]):
                 ax_opt.vlines(x=s, ymin=vmin, ymax=vmax, colors="#b0b0b0", linestyles="-", linewidth=0.8)
                 ax_unopt.vlines(x=s, ymin=vmin, ymax=vmax, colors="#b0b0b0", linestyles="-", linewidth=0.8)
 
-            ax_opt.set_xlim(0, max(splits[i]))
-            ax_unopt.set_xlim(0, max(splits[i]))
+            ax_opt.set_xlim(min(splits[i]), max(splits[i]))
+            ax_unopt.set_xlim(min(splits[i]), max(splits[i]))
             ax_opt.set_ylim(vmin, vmax)
             ax_unopt.set_ylim(vmin, vmax)
             ax_opt.grid(False)
@@ -147,11 +189,23 @@ def create_animation(run_paths: List[str], n_epochs: int, struct: str):
 
 
 if __name__ == "__main__":
+    load_dotenv()
+
+    # run_paths = [
+    #     "logs/multiruns/2022-09-19_22-09-53/0",
+    #     "logs/multiruns/2022-09-19_22-09-53/1",
+    #     "logs/multiruns/2022-09-19_22-09-53/2",
+    #     "logs/multiruns/2022-09-19_22-09-53/3",
+    #     "logs/multiruns/2022-09-19_22-09-53/4",
+    # ]
     run_paths = [
-        "logs/multiruns/2022-09-19_22-09-53/0",
-        "logs/multiruns/2022-09-19_22-09-53/1",
-        "logs/multiruns/2022-09-19_22-09-53/2",
-        "logs/multiruns/2022-09-19_22-09-53/3",
-        "logs/multiruns/2022-09-19_22-09-53/4",
+        "logs/multiruns/2022-09-21_12-32-25/0",
+        "logs/multiruns/2022-09-21_12-32-25/1",
+        "logs/multiruns/2022-09-21_12-32-25/2",
+        "logs/multiruns/2022-09-21_12-32-25/3",
+        "logs/multiruns/2022-09-21_12-32-25/4",
+        "logs/multiruns/2022-09-21_12-32-25/5",
+        "logs/multiruns/2022-09-21_12-32-25/6",
     ]
+
     create_animation(run_paths, n_epochs=10, struct="diag")
